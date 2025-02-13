4a96e4c94c6d87438fafc3033783a5e0c39e0c84
deepakkode <kodadeepak29@gmail.com>
2025-02-13 14:56:49 +0530

    .

--- index.js
+++ index.js
@@ -34,18 +34,38 @@
 
 const express = require('express');
 const { resolve } = require('path');
-
+const data =require('./data.json')
 const app = express();
-const port = 3010;
-
+const port = 3000;
+app.use(express.json());
 app.use(express.static('static'));
 
 app.get('/', (req, res) => {
   res.sendFile(resolve(__dirname, 'pages/index.html'));
 });
 
-app.listen(port, () => {
+app.post( '/students/above-threshold', (req,res) => {
+try {
+  
+ 
+    let threshold= req.body.threshold
+
+    if(!threshold){
+        res.status(400).json({error:"Threshold is required"})
+    }
+    const filteredStudents = data.students.filter(student => student.total > threshold);
+  res.json({
+    count: filteredStudents.length,
+    students: filteredStudents
+  });
+} catch (error) {
+  res.status(500).json({error:error.message})
+}
+}); 
+
+
+
+
+app.listen(port, () => {  
   console.log(`Example app listening at http://localhost:${port}`);
 });
-
-
